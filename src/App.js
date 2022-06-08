09b0d2d863e91bc225e1af991ae48b0853c4dcf4
sureyya <sureyyaamac@gmail.com>
2022-06-09 00:34:25 +0300

    merge

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import React ,{useState,useEffect} from 'react';
-=======
 import React ,{useState} from 'react';
->>>>>>> sryya
 import Header from './components/_header';
 import Footer from './components/_footer';
 import Form from './components/Form';
@@ -26,29 +21,7 @@ function App() {
       
       <Footer selectPage={selectPage} setSelectPage={setSelectPage}/>
       </div>
-=======
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> 6787fb3749e7aadeb1efc3606fd0cfd20cb893e0
-    </div>
-  );
+</div>)
 }
 export default App;
